c88811cca32dc7d7285d88f04c70586cf37c66df
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-05-21 20:23:15 +0300

    feat: add basic layout for catalog page

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,9 +1,18 @@
 import {MainLayout} from "@/widgets";
+import s from './CatalogPage.module.scss'
 
 export function CatalogPage(){
     return (
         <MainLayout>
-            Catalog page
+            <h1 className={s.title}>Каталог услуг</h1>
+            <div className={s.catalogWrapper}>
+                <aside className={s.filtersWrapper}>
+                    <h2>Фильтры</h2>
+                </aside>
+                <div className={s.cardsWrapper}>
+                    карточки
+                </div>
+            </div>
         </MainLayout>
     )
 }
